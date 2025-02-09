1a3c2d830856a0b38c325bc527e87c56a585e89b
VikramSingh138 <vikkram138@gmail.com>
2025-02-09 17:58:43 +0530

    Navbar

--- src/app/AddBootstrap.js
+++ src/app/AddBootstrap.js
@@ -1,11 +1,37 @@
+// "use client";
+// import {useEffect} from 'react';
+
+// export default function AddBootstrap() {
+//     useEffect(() => {
+//         // @ts-ignore
+//         import("bootstrap/dist/js/bootstrap.bundle.min.js");
+//     }, []);
+
+//     return <></>;
+// }
+// "use client";
+// import { useEffect } from "react";
+
+// export default function AddBootstrap() {
+//   useEffect(() => {
+//     if (typeof window !== "undefined") {
+//       require("bootstrap/dist/js/bootstrap.bundle.min.js");
+//     }
+//   }, []);
+
+//   return null; // No need to return an empty fragment
+// }
+
 "use client";
-import {useEffect} from 'react';
+import { useEffect } from "react";
 
 export default function AddBootstrap() {
-    useEffect(() => {
-        // @ts-ignore
-        import("bootstrap/dist/js/bootstrap.bundle.min.js");
-    }, []);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }
+  }, []);
 
-    return <></>;
+  return null; // No need to return an empty fragment
 }
+
